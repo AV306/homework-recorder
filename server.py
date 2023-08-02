961693bc6e4e5c39035d3eb1a441197e716e399c
AV3_08 <brandonjoash@gmail.com>
2023-08-02 05:56:35 +0000

    some code (im glad i did this last year)

--- server.py
+++ server.py
@@ -10,38 +10,22 @@ def onIndexGetOrPost():
 	"""Handle HTTP requests to the main route."""
 	if request.method == 'GET':
 		# send the submit page
-		return render_template( 'index.html' );
+		return render_template( "index.html" );
 		
 	elif request.method == 'POST':
 		# form data
+		# TODO: put in new form data ids
 		name = request.form["name_input"];
 		reason = request.form["reason_input"];
 		details = request.form["details_input"];
 		password = request.form["password_input"];
-		return render_template( "index.html", auth=api.send( name, reason, details, password ) );
-
-
-@app.route('/api', methods=['GET'])
-def onApiGet():
-	"""Handle GET requests to the data route."""
-	return api.retrieve( request.args );
-
-
-@app.route('/data', methods=['GET'])
-def onDataGet():
-	"""Return a human-readable version of the api data."""
-	# retrieve the api data.
-	data = api.retrieve( request.args );
-
-	# format the data into a human-readable format.
-
-	return render_template( "data_table.html", data=data );
+		render_template( "index.html" );
 
 
 @app.errorhandler(404)
 def notFound( error ):
-	return render_template( "errors/error.html", error=error ), 404
+	return render_template( "errors/error.html", error=error, code=404 ), 404
 
 @app.errorhandler(500)
 def serverError( error ):
-	return render_template( "errors/error.html", error=error ), 500
+	return render_template( "errors/error.html", error=error, code=500 ), 500
